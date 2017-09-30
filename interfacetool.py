bcbc12acda9eb5b182aea3096571162faf0eb60f
Victor <marksinoberg@gmail.com>
2017-09-29 20:57:25 -0500

    Update interfacetool.py

--- interfacetool.py
+++ interfacetool.py
@@ -1,21 +1,35 @@
+#!/usr/bin python
 # coding: utf8
-
-# @Author: 郭 璞
-# @File: exectool.py                                                                 
-# @Time: 2017/8/26                                   
-# @Contact: 1064319632@qq.com
-# @blog: http://blog.csdn.net/marksinoberg
-# @Description: 调试助手工具
+# Vim Runtime: /usr/share/vim/vim74
+# File Name: exectools.py
+# Author:  郭璞 
+# Create Time: Sat 26 Aug 2017 09:18:46 PM CST
+# Blog: http://blog.csdn.net/marksinoberg
 
 import subprocess
-import requests
-cmd = ""
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+import prettytable
+from prettytable import PrettyTable as PT
+import sys
+from colorama import init, Fore, Back
 
+#cmd = "wget http://fanyi.badu.com/v2transapi?query=hello | python -m json.tool"
+
+cmd = "php {} | python -m json.tool".format(sys.argv[1])
+
+p = subprocess.Popen(["php test.php | python -m json.tool", "python -m json.tool"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+result = ""
 while True:
-    data = p.stdout.readline()
-    if data == b'':
-        if p.poll() is not None:
-            break
-        else:
-            print(data)
+	data = p.stdout.readline()
+	if data == b'':
+		break
+	else:
+	    result += data.decode('ascii').encode('utf8').decode('utf8')
+
+table = PT(["文件名称", "执行结果"])
+init(autoreset=True)
+filename = Fore.YELLOW + sys.argv[1]+''
+table.add_row([filename, Fore.GREEN+result])
+table.align='l'
+print table
+
