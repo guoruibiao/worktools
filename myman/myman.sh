1cfda148a2e7f9337a90d17e2debbc33ea7642aa
Victor <marksinoberg@gmail.com>
2018-06-15 19:04:45 +0800

    Update myman.sh

--- myman/myman.sh
+++ myman/myman.sh
@@ -29,7 +29,16 @@ elif test "$1" == "list";then
     REPO_AVAIABLE_COMMANDS=`curl -s $REPO_CMDS_URL`
     echo "云端可用命令文档:"
     echo $REPO_AVAIABLE_COMMANDS
-    echo "列出还可以下载的云端命令列表吧TODO"
+    echo "TODO: array diff in bash."
+elif test "$1" == "search";then
+    IN_REPO=`curl -s $REPO_CMDS_URL | grep $2`
+    if test "$IN_REPO" == "$2";then
+        curl -s $CMD_DOWNLOAD_URL$2 | less
+        echo "[$2] 命令可以下载，简要内容如下:"
+    else
+        echo "[$2] 命令文档暂不支持，期待您的贡献与参与哦~"
+    fi
 else
     echo "Command ["$1"] not found." 
 fi
+
