ca0dac1cc0f480aa3a5d8ff7983d6e6e78f00050
guoruibiao <marksinoberg@gmail.com>
2019-12-17 12:00:49 +0800

    新增提交注释

--- svndiff.sh
+++ svndiff.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# 查看SVN管理下某文件最近一次被改动的内容
-# svn log xxx -l 2
-# svn diff xxx -r old_version:new_version
-
 # 当前工作目录，备用
 CURRENT_WORKSPACE=`pwd`
 # 待对比对象，可以是具体的文件名，也可以是目录， TODO 处理为空的case
@@ -14,8 +10,10 @@ NEW_OPERATOR=`svn log $DIFF_OBJECT -l 2 | grep -o "^r.*" | awk -F'|' '{print sub
 NEW_VERSION_NUMBER=`svn log $DIFF_OBJECT -l 2 | grep -o "^r.*" | awk -F'|' '{print substr($1, 2, length($1)), $2}' | head -1 | awk '{print $1}'`
 OLD_OPERATOR=`svn log $DIFF_OBJECT -l 2 | grep -o "^r.*" | awk -F'|' '{print substr($1, 2, length($1)), $2}' | head -2| tail -1 | awk '{print $2}'`
 OLD_VERSION_NUMBER=`svn log $DIFF_OBJECT -l 2 | grep -o "^r.*" | awk -F'|' '{print substr($1, 2, length($1)), $2}' | head -2| tail -1 | awk '{print $1}'`
+COMMENT=`svn log $DIFF_OBJECT -l 1 | grep @`
 
 # 格式化输出内容
 echo "操作人[${NEW_OPERATOR}@${NEW_VERSION_NUMBER}] ---> 被操作人[${OLD_OPERATOR}@${OLD_VERSION_NUMBER}]"
-echo "详细改动为"
+echo "详细改动为:  $COMMENT"
+
 svn diff $DIFF_OBJECT -r ${OLD_VERSION_NUMBER}:${NEW_VERSION_NUMBER}
